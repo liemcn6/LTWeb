82e980cd2c34c9cbd1ad955ffa92e908be01103b
liemcn6 <quangliem743@gmail.com>
2022-12-19 17:15:13 +0700

    add

---
+++ 1/category/lienhe/index.php
@@ -0,0 +1,123 @@
+<?php
+	$title = 'Liên hệ';
+	$baseUrl = '../../';
+    include_once ($baseUrl.'FE/layouts_FE/header.php');
+?>
+<title>LIÊN HỆ</title>
+<style>
+        .wide-nav-items-span {
+            color: #777;
+        }
+        .danhmuc-lienhe {
+            color: #000;
+        }
+</style>
+<div class="lienhe-box">
+    <div class="lienhe-body">
+        <div class="lienhe-top-box">
+            <div class="lienhe-top">
+                <div class="lienhe-connect">
+                    <div class="lienhe-connect-title">
+                        <span>Hãy Kết Nối Với Chúng Tôi</span>
+                    </div>
+                    <div class="lienhe-connect-icon-box">
+                        <div class="lienhe-connect-icon lienheFB">
+                            <a href="" class="tag-a"><i class="fab fa-facebook-f"></i></a>
+                            <div class="header-social-hover lienhe-connect-icon-hover">
+                                <span>Follow on Facebook</span>
+                                <div class="hover-square lienhe-connect-icon-square"></div>
+                            </div>
+                        </div>
+                        <div class="lienhe-connect-icon lienhe-ITG">
+                            <a href="" class="tag-a"><i class="fab fa-instagram"></i></a>
+                            <div class="header-social-hover lienhe-connect-icon-hover">
+                                <span>Follow on Instagam</span>
+                                <div class="hover-square lienhe-connect-icon-square"></div>
+                            </div>
+                        </div>
+                        <div class="lienhe-connect-icon lienhe-TWWITER ">
+                            <a href="" class="tag-a"><i class="fab fa-twitter"></i></a>
+                            <div class="header-social-hover lienhe-connect-icon-hover">
+                                <span>Follow on Twitter</span>
+                                <div class="hover-square lienhe-connect-icon-square"></div>
+                            </div>
+                        </div>
+                        <div class="lienhe-connect-icon lienhe-PHONE ">
+                            <a href="" class="tag-a"><i class="fas fa-phone-alt"></i></a>
+                            <div class="header-social-hover lienhe-connect-icon-hover lienhe-connect-icon-hover-phone">
+                                <span>0378208856</span>
+                                <div class="hover-square lienhe-connect-icon-square"></div>
+                            </div>
+                        </div>
+                        <div class="lienhe-connect-icon lienhe-YT ">
+                            <a href="" class="tag-a"><i class="fab fa-youtube"></i></a>
+                            <div class="header-social-hover lienhe-connect-icon-hover">
+                                <span>Follow on Youtube</span>
+                                <div class="hover-square lienhe-connect-icon-square"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="lienhe-mid">
+            <div class="lienhe-mid-items">
+                <div class="lienhe-mid-img-box">
+                    <img src="<?=$baseUrl?>FE/img/lienhe-mid1.jpg" alt="">
+                </div>
+                <div class="lienhe-mid-city">
+                    <span>HÀ NỘI</span>
+                </div>
+                <div class="lienhe-mid-street">
+                    <span>Số 121 đường Cầu Giấy</span>
+                </div>
+                <div class="lienhe-mid-district">
+                    <span>Quận Cầu Giấy, Thành Phố Hà Nội</span>
+                </div>
+                <div class="lienhe-mid-phone">
+                    <span>+ SĐT: 0996693396</span> 
+                </div>
+                <div class="lienhe-mid-email">
+                    <span>+ Email: vuloc1810@gmail.com</span>
+                </div>
+            </div>
+            <div class="lienhe-mid-items">
+                <div class="lienhe-mid-img-box">
+                    <img src="<?=$baseUrl?>FE/img/lienhe-mid2.jpg" alt="">
+                </div>
+                <div class="lienhe-mid-city">
+                    <span> HỒ CHÍ MINH</span>
+                </div>
+                <div class="lienhe-mid-street">
+                    <span>6A3, Phạm Ngũ Lão</span>
+                </div>
+                <div class="lienhe-mid-district">
+                    <span>Quận 1, Thành phố Hồ Chí Minh</span>
+                </div>
+                <div class="lienhe-mid-phone">
+                    <span>+ SĐT: 091.345.2502</span> 
+                </div>
+                <div class="lienhe-mid-email">
+                    <span>+ Email: vuloc11A3MĐA@gmail.com</span>
+                </div>
+            </div>
+        </div>
+        <div class="lienhe-bottom-box">
+            <div class="lienhe-bottom">
+                <div class="lienhe-address-title">
+                    <hr class="lienhe-hr">
+                    <span>VỊ TRÍ CỬA HÀNG TRÊN BẢN ĐỒ</span>
+                    <hr class="lienhe-hr">
+                </div>
+                <div class="lienhe-map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8858.70173270601!2d105.83772665588455!3d21.007653359756837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac76ccab6dd7%3A0x55e92a5b07a97d03!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBCw6FjaCBraG9hIEjDoCBO4buZaQ!5e0!3m2!1svi!2s!4v1635151693471!5m2!1svi!2s" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+	require_once($baseUrl.'FE/layouts_FE/footer.php');
+?>
+<script src="<?=$baseUrl?>FE/js/cuahang.js"></script>
+<script src="<?=$baseUrl?>FE/js/index.js"></script>
